d50b310617bea03a8f48ddb99758fd90c69b778d
kobanda <george17156@fmail.com>
2025-05-29 12:15:51 +0300

    Describe what you changed

--- app-streamlit.py
+++ app-streamlit.py
@@ -1,7 +1,7 @@
 #imports
 import pandas as pd
 import streamlit as st
-#import plotly_express as px
+import plotly_express as px
 
 #making a project in Titanic dashboard
 st.set_page_config(layout="wide")
