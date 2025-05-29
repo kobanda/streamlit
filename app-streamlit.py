7315ff1b154dc0d0b209e53765e4e38d545c9674
kobanda <george17156@fmail.com>
2025-05-29 15:08:03 +0300

    Describe what you change

--- app-streamlit.py
+++ app-streamlit.py
@@ -7,4 +7,18 @@ import plotly_express as px
 st.set_page_config(layout="wide")
 st.title("Titanic Dashboard")
 
-# the
+# bring in titanic data
+df = pd.read_csv('titani_data.csv')
+#st.write(df.head())
+
+#fill column called embarked with unknow for missing values
+df.Embarked = df.Embarked.fillna('Unknown')
+embarked_port =list( df.Embarked.unique())
+gender = list(df.Sex.unique())
+
+#create two colums
+col1, col2 = st.columns([1,1])
+with col1:
+    st.selectbox(options = embarked_port,label="Select a Port")
+with col2:
+    st.selectbox(options = gender,label="Select a Gender")
