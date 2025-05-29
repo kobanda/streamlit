af5dfdfb38b350da63b3f8c7b7bbe0c3b6c6dfba
kobanda <george17156@fmail.com>
2025-05-29 16:27:26 +0300

    Describe what you change

--- app-streamlit.py
+++ app-streamlit.py
@@ -1,4 +1,6 @@
 #imports
+from itertools import groupby
+
 import pandas as pd
 import streamlit as st
 import plotly_express as px
@@ -18,7 +20,31 @@ gender = list(df.Sex.unique())
 
 #create two colums
 col1, col2 = st.columns([1,1])
-with col1:
-    st.selectbox(options = embarked_port,label="Select a Port")
-with col2:
-    st.selectbox(options = gender,label="Select a Gender")
+selected_port = col1.selectbox(options = embarked_port,label="Select a Port")
+selected_gender=col2.selectbox(options = gender,label="Select a Gender")
+
+#filter using the selected port and gender
+df_plot = df[df.Embarked==selected_port]
+df_plot = df_plot[df.Sex==selected_gender]
+#st.write(df_plot.head())
+
+#plot
+plot = px.histogram(data_frame=df_plot,
+                    template="seaborn",
+                    color="Survived",
+                    title="Distribution of Age",
+                    facet_col="Survived",
+                    x="Age"
+                    )
+(col1.plotly_chart(plot))
+df_plot_pie= df_plot.loc[:,['PassengerId','Survived']].groupby(['Survived']).count()
+
+df_plot_pie.rename({'PassengerId':'Count of Passengers'},inplace=True,axis='columns')
+st.write(df_plot_pie)
+pie = px.pie(data_frame=df_plot_pie,
+             template="seaborn",
+             values="Count of Passengers",
+             title="Count of Passengers that survived",
+             names="Survived"
+             )
+st.plotly_chart(pie)
