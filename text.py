cb2b621896b5528c66081182d326de3d179d5891
kobanda <george17156@fmail.com>
2025-05-30 13:39:40 +0300

    Describe what you change

--- text.py
+++ text.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from requests import options
+from select import select
 
 st.title("Streamlit text Widget Lesson")
 col1,col2 = st.columns([1,1])
@@ -15,10 +17,29 @@ text_passowrd = col2.text_input(label = "Enter your Password",
                              key = "tex_password"
                              )
 #the text area
-text_area = st.text_area(label = "This is text area",
+text_area = col1.text_area(label = "This is text area",
                          max_chars=1000,
                          placeholder = "Write your text here",
                          key="text_area",
                          help = "This is multi-line text area"
                          )
 
+#lets do number input
+number_input = st.sidebar.number_input(label="Enter a number",
+                               max_value=100,
+                               min_value=0,
+                               value=50,
+                               step=1,
+                               help="Use this widget to input a number"
+                               )
+st.sidebar.write(f"The number is: ",number_input)
+#lets now learn pills
+cities = ["Siaya","Nairobi","Kisumu","Mombasa","Nakuru","London","America"]
+selected_city = col2.pills(label="Select a city",
+                 options=cities,
+                 selection_mode="multi",
+                 help="Pick your faviourite city",
+                 key="city1"
+                 )
+#print the selected city
+col2.write("The city selected is: {}".format(selected_city))
